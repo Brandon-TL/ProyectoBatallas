0e60f4d7ee1db9ea373c82e7a42d4c051745c754
brandon-tl <brandon.timeleaper@gmail.com>
2023-01-17 21:01:43 +0100

    provisional IV

--- provisional/index.php
+++ provisional/index.php
@@ -5,31 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php
+        require_once './php/visual.php';
         if (isset($_SESSION["usuario"])) {
             header("Location: perfil.php");
             exit();
         }
     ?>
-    <title>Login</title>
+    <title><?php echo $lang['loginTitle'] ?></title>
 </head>
-<body>
     <?php
         require_once './php/funciones.php';
         require_once './php/logica_index.php';
     ?>
+    <header>
+        <div class="selectors">
+            <div class="dropdown" title="Select language">
+                <button><?php echo $lang['selectLang']; ?></button>
+                <ul>
+                    <li><a href="index.php?lang=en"><div class="flag en"></div><?php echo $lang['lang_en'] ?></a></li>
+                    <li><a href="index.php?lang=es"><div class="flag es"></div><?php echo $lang['lang_es'] ?></a></li>
+                </ul>
+            </div>
+            <form action="" method="POST" class="theme-form">
+                <button name="tema" value="dark" class="dark"><ion-icon name="moon-outline"></ion-icon></button>
+                <button name="tema" value="light" class="light"><ion-icon name="sunny-outline"></ion-icon></button>
+            </form>
+        </div>
+    </header>
+    <h1><?php echo $lang['tituloLogin'] ?></h1>
     <form action="" method="post" class="containerForm pre" enctype="multipart/form-data">
         <div>
-            <label for="usuario" class="nonClickable">Nombre de usuario</label>
+            <label for="usuario" class="nonClickable"><?php echo $lang['nombreLogin'] ?></label>
             <input type="text" name="usuario">
+            <?php if (isset($_usuario_err)) echo $_usuario_err; ?>
         </div>
 
         <div>
-            <label for="password" class="nonClickable">Contraseña</label>
+            <label for="password" class="nonClickable"><?php echo $lang['passwordLogin'] ?></label>
             <input type="password" name="password">
+            <?php if (isset($_password_err)) echo $_password_err; ?>
         </div>
         
-        <a href="registro.php">¿Aún no tienes cuenta?</a><br>
-        <input type="submit" name="ENTRAR" value="ACCEDER" class="loginButton btn">
+        <a href="registro.php"><?php echo $lang['indexPreg2'] ?></a><br>
+        <input type="submit" name="ENTRAR" value="<?php echo $lang['btnEntrarLogin'] ?>" class="loginButton btn">
     </form>
 </body>
 </html>
--- provisional/registro.php
+++ provisional/registro.php
@@ -4,38 +4,71 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de usuario</title>
+    <?php
+        require_once './php/visual.php';
+        session_start();
+        if (isset($_SESSION["usuario"])) {
+            header("Location: perfil.php");
+            exit();
+        }
+    ?>
+    <title><?php echo $lang['registroTitle'] ?></title>
 </head>
 <body>
+    <?php
+        require_once 'php/funciones.php';
+        require_once 'php/logica_registro.php';
+    ?>
+    <header>
+        <div class="selectors">
+            <div class="dropdown" title="Select language">
+                <button><?php echo $lang['selectLang']; ?></button>
+                <ul>
+                    <li><a href="registro.php?lang=en"><div class="flag en"></div><?php echo $lang['lang_en'] ?></a></li>
+                    <li><a href="registro.php?lang=es"><div class="flag es"></div><?php echo $lang['lang_es'] ?></a></li>
+                </ul>
+            </div>
+            <form action="" method="POST" class="theme-form">
+                <button name="tema" value="dark" class="dark"><ion-icon name="moon-outline"></ion-icon></button>
+                <button name="tema" value="light" class="light"><ion-icon name="sunny-outline"></ion-icon></button>
+            </form>
+        </div>
+    </header>
     <form action="" method="post" class="containerForm pre" enctype="multipart/form-data">
         <div>
             <label for="usuario">Nombre de usuario</label>
-            <input type="text" name="usuario" value="">
+            <input type="text" name="usuario" value="<?php echo $_usuario;?>">
+            <?php echo $_usuario_err; ?>
         </div>
 
         <div>
             <label for="password">Contraseña</label>
-            <input type="password" name="password" value="">
+            <input type="password" name="password" value="<?php echo $_password; ?>">
+            <?php echo $_password_err; ?>
         </div>
 
         <div>
             <label for="password2">Repita su contraseña</label>
-            <input type="password" name="password2" value="">
+            <input type="password" name="password2" value="<?php echo $_password; ?>">
+            <?php echo $_password2_err; ?>
         </div>
         
         <div>
             <label for="email">E-mail</label>
-            <input type="email" name="email" value="">
+            <input type="email" name="email" value="<?php echo $_email; ?>">
+            <?php echo $_email_err; ?>
         </div>
         
         <div>
             <label for="fecha">Fecha de nacimiento</label>
-            <input type="date" name="fecha" value="">
+            <input type="date" name="fecha" value="<?php echo $_fecha; ?>">
+            <?php echo $_fecha_err; ?>
         </div>
         
         <div>
             <label for="avatar">Foto de perfil</label>
-            <input type="file" name="avatar" accept="image/png, image/jpeg, image/jpg" value="">
+            <input type="file" name="avatar" accept="image/png, image/jpeg, image/jpg" value="<?php echo $_avatar; ?>">
+            <?php echo $_avatar_err; ?>
         </div>
         
         <input type="button" value="VOLVER" class="registroButton btn" onclick="window.location.href='index.php'">
