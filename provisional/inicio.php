ecf17cd30d55334eeda27ae14f5529a2ed3aa56b
brandon-tl <brandon.timeleaper@gmail.com>
2023-01-18 11:14:04 +0100

    provisional VI

--- provisional/inicio.php
+++ provisional/inicio.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+        require_once './php/visual.php';
+        if (isset($_SESSION["usuario"])) {
+            header("Location: perfil.php");
+            exit();
+        }
+    ?>
     <title>Inicio</title>
 </head>
 <body>
+    <header>
+        <div class="selectors">
+            <div class="dropdown" title="Select language">
+                <button><?php echo $lang['selected_lang']; ?></button>
+                <ul>
+                    <li><a href="registro.php?lang=en"><div class="flag en"></div><?php echo $lang['lang_en'] ?></a></li>
+                    <li><a href="registro.php?lang=es"><div class="flag es"></div><?php echo $lang['lang_es'] ?></a></li>
+                </ul>
+            </div>
+            <form action="" method="POST" class="theme-form">
+                <button name="tema" value="dark" class="dark"><ion-icon name="moon-outline"></ion-icon></button>
+                <button name="tema" value="light" class="light"><ion-icon name="sunny-outline"></ion-icon></button>
+            </form>
+        </div>
+    </header>
     <h1>Estas en inicio</h1>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
