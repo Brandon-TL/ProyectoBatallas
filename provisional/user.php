23babdfe69201c917a6cc93fcd4458f53353f65b
brandon-tl <brandon.timeleaper@gmail.com>
2023-01-23 03:22:42 +0100

    .

--- provisional/user.php
+++ provisional/user.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/user.css">
     <link rel="stylesheet" href="./css/ajustes.css">
+    <link rel="stylesheet" href="./css/como.css">
     <?php
         require_once './php/logica_perfil.php';
         require_once './php/visual.php';
