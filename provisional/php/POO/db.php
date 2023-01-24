62254dca41ea574c9b366cfcdeb52329d172adeb
brandon-tl <brandon.timeleaper@gmail.com>
2023-01-25 00:00:23 +0100

    .

--- provisional/php/POO/db.php
+++ provisional/php/POO/db.php
@@ -4,16 +4,53 @@
         const USER = "root";
         const PASSWORD = "";
         const DBNAME = "dbbatallas";
-
+        private $sentencia;
         private $conexion;
 
-        public function connect () {
+        /**
+         * Función que conecta con la base de datos
+         */
+        public function abrirConexion () {
             $this->conexion = new PDO('mysql:host='.DB::HOST.';dbname='.DB::DBNAME, DB::USER, DB::PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        
+        /**
+         * Función que desconecta de la base de datos
+         */
+        private function cerrarConexion () {
+            $this->conexion = null;
+        }
+            
+        /**
+         * Función para ejecutar un query simple del tipo INSERT, DELETE, UPDATE !!! NO Select
+         * @param  string:sql sentencia sql
+         */
+        public function ejecutar ($sql) {
+            $this->conexion = $sql;
+            $this->abrirConexion();
+            $this->conexion->query($this->sentencia);
+            $this->cerrarConexion();
+        }
+
+        /**
+         * Método que traer resultados de una consulta en un Array
+         * @param  string:sql sentencia sql
+         * 
+         * @return array:resultado con los valores de la BBDD que cumplen la query
+         */
+        public function obtenerResultado($sql){
+            $this->sentencia = $sql;
+            $this->abrirConexion();
+            $resultado = array();
+
+            foreach ($this->conexion->query($this->sentencia) as $row) {
+                array_push($resultado, $row);
+            }
+            
+            return $resultado;
         }
 
         public function registrarUsuario (user $usuario) {
-            $this->conexion->beginTransaction();
-            
             $sql = "INSERT INTO `usuario`(`fechanacimiento`, `foto`, `email`, `modovis`, `idioma`, `rol`, `num_elementos_creados`, `num_batallas_creadas`, `num_batallas_votadas`, `num_batallas_ignoradas`, `num_batallas_denunciadas`, `puntos_troll`) VALUES ($usuario->fecha, $usuario->foto, $usuario->email, $usuario->tema, $usuario->idioma, 'usuario', 0, 0, 0, 0, 0, 0)";
             echo $sql;
             
@@ -21,17 +58,13 @@
                 $sql = "INSERT INTO `credencial`(`nombreusuario`, `password`) VALUES ($usuario->nombre, $usuario->password)";
                 echo $sql;
                 if ($this->conexion->ejecutar($sql)) {
-                    $this->conexion->commit();
+                    echo "Usuario registrado";
                 } else {
-                    $this->conexion->rollBack();
+                    echo "No se ha podido  registrar el usuario";
                 }
             } else {
-                $this->conexion->rollBack();
+                echo "No se ha podido  registrar el usuario";
             }
-        }
-
-        public function ejecutar($sql){
-            $this->conexion->query($sql);
         }
     }
 ?>
--- provisional/php/POO/test.php
+++ provisional/php/POO/test.php
@@ -9,17 +9,8 @@
     require_once 'db.php';
 
     $luis = new user('', 'Luisetepete', 'luis@gmail.com', 'img/luis.jpg', 'dark', 'es', 'dawdawdaw123');
-    echo $luis->nombre;
-    echo '<br><br>';
-    
     $luis->__set('nombre', 'Luiseta');
-    echo $luis->nombre;
-    echo '<br><br>';
-    
-    $email = $luis->__get('email');
-    echo $email;
-    echo '<br><br>';
-    
-    $conexion = new db();
+
+    $conexion = new db;
     $conexion->registrarUsuario($luis);
 ?>
